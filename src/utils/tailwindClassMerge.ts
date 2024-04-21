369712215046ef57b7ff6d6c8513a41330d86b66
Salvador Bacci <salvador_bacci@hotmail.com>
2024-04-21 03:49:22 -0300

    added tailwind-merge prefix configuration

--- src/utils/tailwindClassMerge.ts
+++ src/utils/tailwindClassMerge.ts
@@ -1,5 +1,9 @@
 import { type ClassValue, clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import { extendTailwindMerge } from 'tailwind-merge'
+
+const twMerge = extendTailwindMerge({
+  prefix: 'fr-' // need to be same as per tailwind.config.js
+})
 
 /**
  * Merge conflicting tailwind classes
