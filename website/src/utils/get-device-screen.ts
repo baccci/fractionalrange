d4cd95a67eb6f0df626cc1310d15cd7c5dcd87a0
Salvador Bacci <salvador_bacci@hotmail.com>
2026-03-23 02:36:55 -0300

    Remove unused DynamicLine and FractionalRange components, and clean up getDeviceScreen and notANumber utility functions

--- website/src/utils/get-device-screen.ts
+++ website/src/utils/get-device-screen.ts
@@ -1,10 +1,5 @@
 import { MOBILE_WIDTH_BREAKPOINT, TABLET_WIDTH_BREAKPOINT } from '@/constants/screen'
 
-/**
- * Retrieves information about the device screen.
- * @returns An object containing the width, height, and device type of the screen.
- * @throws {Error} If the function is executed outside of a browser environment.
- */
 export function getDeviceScreen() {
   if (typeof window === 'undefined') throw new Error('This function must be executed in the browser')
 
@@ -13,11 +8,6 @@ export function getDeviceScreen() {
     width: innerWidth,
     height: innerHeight,
     isMobile: innerWidth < MOBILE_WIDTH_BREAKPOINT,
-    isTablet: innerWidth >= MOBILE_WIDTH_BREAKPOINT && innerWidth < TABLET_WIDTH_BREAKPOINT,
-    isDesktop: innerWidth >= TABLET_WIDTH_BREAKPOINT,
-    isSmallerThanTablet: innerWidth < TABLET_WIDTH_BREAKPOINT,
-    isSmallerThanDesktop: innerWidth < TABLET_WIDTH_BREAKPOINT,
     isSmallerOrEqualToTablet: innerWidth <= TABLET_WIDTH_BREAKPOINT,
-    isSmallerOrEqualToDesktop: innerWidth <= TABLET_WIDTH_BREAKPOINT
   }
 }
